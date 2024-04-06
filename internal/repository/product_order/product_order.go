b99647772f7fd18834ab2f0ca9bfdc7c1e414775
Dikontay <student@ALEM-F3-L07-05.alem.school>
2024-04-06 20:56:54 +0500

    err : pointer dirreference

--- internal/repository/product_order/product_order.go
+++ internal/repository/product_order/product_order.go
@@ -11,18 +11,45 @@ type ProductOrderStorage struct {
 	db *sql.DB
 }
 
-//fk product id
-//fr order_id
+type ProductOrderRepo interface {
+	GetProductOrderByID(orderID int) (*models.ProductOrder, error)
+	GetProductOrdersByIDs(orderIDs []int) ([]*models.ProductOrder, error)
+}
 
 func NewProductOrderStorage(db *sql.DB) *ProductOrderStorage {
 	return &ProductOrderStorage{db: db}
+}
+
+func (s *ProductOrderStorage) GetProductOrderByID(orderID int) (*models.ProductOrder, error) {
+	queryProductOrder := `SELECT Product_order_id, order_id, product_id, quantity FROM product_order WHERE order_id = ?`
+
+	row := s.db.QueryRow(queryProductOrder, orderID)
+
+	var id, productID, quantity int
+
+	if err := row.Scan(&id, &orderID, &productID, &quantity); err != nil {
+		return nil, err
+	}
+
+	p := &models.ProductOrder{
+		ID:        id,
+		ProductID: productID,
+		OrderID:   orderID,
+		Quantity:  quantity,
+	}
+
+	if err := row.Err(); err != nil {
+		return nil, err
+	}
+
+	return p, nil
 }
 
 func (s *ProductOrderStorage) GetProductOrdersByIDs(orderIDs []int) ([]*models.ProductOrder, error) {
 	placeholders := strings.Repeat("?,", len(orderIDs)-1) + "?"
 	fmt.Println(placeholders)
 
-	queryProductOrder := fmt.Sprintf(`SELECT product_id, order_id, quantity FROM product_order WHERE order_id IN (%s)`, placeholders)
+	queryProductOrder := fmt.Sprintf(`SELECT Product_order_id, product_id, order_id, quantity FROM product_order WHERE order_id IN (%s)`, placeholders)
 
 	// Подготавливаем аргументы для запроса
 	args := make([]interface{}, len(orderIDs))
@@ -38,11 +65,12 @@ func (s *ProductOrderStorage) GetProductOrdersByIDs(orderIDs []int) ([]*models.P
 	defer rows.Close()
 	var productOrders []*models.ProductOrder
 	for rows.Next() {
-		var productID, orderID, quantity int
-		if err := rows.Scan(&productID, &orderID, &quantity); err != nil {
+		var id, productID, orderID, quantity int
+		if err := rows.Scan(&id, &productID, &orderID, &quantity); err != nil {
 			return nil, err
 		}
 		productOrders = append(productOrders, &models.ProductOrder{
+			ID:        id,
 			ProductID: productID,
 			OrderID:   orderID,
 			Quantity:  quantity,
@@ -54,28 +82,4 @@ func (s *ProductOrderStorage) GetProductOrdersByIDs(orderIDs []int) ([]*models.P
 	}
 
 	return productOrders, nil
-}
-
-func (s *ProductOrderStorage) GetProductOrderByID(orderID int) (*models.ProductOrder, error) {
-	queryProductOrder := `select product_id, quantity from product_order where order_id = ?`
-
-	row := s.db.QueryRow(queryProductOrder, orderID)
-
-	var productID, quantity int
-
-	if err := row.Scan(&productID, &quantity); err != nil {
-		return nil, err
-	}
-
-	p := &models.ProductOrder{
-		ProductID: productID,
-		OrderID:   orderID,
-		Quantity:  quantity,
-	}
-
-	if err := row.Err(); err != nil {
-		return nil, err
-	}
-
-	return p, nil
 }
--- internal/repository/product_order/product_order_test.go
+++ internal/repository/product_order/product_order_test.go
@@ -1,1 +1,82 @@
 package product_order
+
+import (
+	"ecommerce/internal/models"
+	"ecommerce/utils"
+	"fmt"
+	"reflect"
+	"regexp"
+	"strings"
+	"testing"
+
+	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/stretchr/testify/assert"
+)
+
+var productOrder = &models.ProductOrder{
+	ID:        2,
+	OrderID:   1,
+	ProductID: 2,
+	Quantity:  3,
+}
+
+func TestGetProductOrderByID(t *testing.T) {
+	db, mock := utils.NewMock()
+
+	repo := NewProductOrderStorage(db)
+
+	defer db.Close()
+
+	query := `SELECT Product_order_id, order_id, product_id, quantity FROM product_order WHERE order_id = ?`
+
+	rows := sqlmock.NewRows([]string{"Product_order_id", "product_id", "order_id", "quantity"}).
+		AddRow(productOrder.ID, productOrder.OrderID, productOrder.ProductID, productOrder.Quantity)
+
+	mock.ExpectQuery(regexp.QuoteMeta(query)).WithArgs(productOrder.OrderID).WillReturnRows(rows)
+	p, err := repo.GetProductOrderByID(productOrder.OrderID)
+	assert.NotNil(t, p)
+	assert.NoError(t, err)
+}
+
+func TestGetProductOrdersByIDs(t *testing.T) {
+	db, mock := utils.NewMock()
+
+	repo := NewProductOrderStorage(db)
+	defer db.Close()
+
+	orderIDs := []int{1, 2}
+
+	placeholders := strings.Repeat("?,", len(orderIDs)-1) + "?"
+	rows := sqlmock.NewRows([]string{"Product_order_id", "product_id,", "order_id", "quantity"}).
+		AddRow(1, 1, 2, 5).
+		AddRow(2, 2, 2, 4)
+
+	queryProductOrder := fmt.Sprintf(`SELECT Product_order_id, product_id, order_id, quantity FROM product_order WHERE order_id IN (%s)`, placeholders)
+
+	mock.ExpectQuery(regexp.QuoteMeta(queryProductOrder)).WillReturnRows(rows)
+
+	productOrders, err := repo.GetProductOrdersByIDs(orderIDs)
+	if err != nil {
+		t.Errorf("error was not expected while getting data from product_order table %s", err)
+	}
+
+	expected := []models.ProductOrder{{
+		ID:        1,
+		OrderID:   1,
+		ProductID: 2,
+		Quantity:  5,
+	}, {
+		ID:        2,
+		OrderID:   2,
+		ProductID: 2,
+		Quantity:  4,
+	}}
+
+	if !reflect.DeepEqual(*productOrders, expected) {
+		t.Errorf("expected %#v, got %#v", expected, productOrders)
+	}
+
+	if err := mock.ExpectationsWereMet(); err != nil {
+		t.Errorf("expectations: %s", err)
+	}
+}
